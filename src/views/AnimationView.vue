365419f6ea52bc82d2571d316dfa77acda8e7dc6
michaeldriscoll <michaelndriscoll81@gmail.com>
2022-11-23 20:13:04 -0500

    testing out various css tools on animations page

---
+++ src/views/AnimationView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="animation">
+      <h1>This is an animation page</h1>
+    </div>
+    <h3>sfah</h3>
+  </template>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+h1 {
+  color: red;
+  transition: transform .7s ease-in-out;
+}
+h1:hover {
+    color: greenyellow;
+    transform: rotate(360deg);
+}
+h3 {
+    font-size: 60px;
+}
+h3:hover {
+    color: blue;
+    font-size: 45px;
+}
+</style>
