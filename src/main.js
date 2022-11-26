34b5b5c9fee4e484432bf16bb3036345badce55e
michaeldriscoll <michaelndriscoll81@gmail.com>
2022-11-25 19:31:15 -0500

    added code to main.js so axios can be used globally

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+
+app = new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
 
 createApp(App).use(router).mount('#app')
