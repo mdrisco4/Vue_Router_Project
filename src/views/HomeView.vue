b0293501a4ab25be6eb03f76401ae492851a88d5
michaeldriscoll <michaelndriscoll81@gmail.com>
2022-12-01 20:51:18 -0500

    added in some commented out code from an API for crypto prices (including the key to be moved later) need to continue figurng out hwo to make a proper call tomorrow.  Will parse data when that works and build out a pgae with prices.  Other oages will be a landing page, an info page and another page undetermined at this point.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,14 @@
   <div class="home">
     <h1>Users Data</h1>
     <ul class="item" v-for="item in list" :key="item.id">
-    <li>{{item.id}}</li>
-    <li>{{item.email}}</li>
-    <li>{{item.first_name}} {{item.last_name}}</li>
-    <li>
-      <img :src="item.avatar" />
-    </li>
-  </ul>
-  <!-- <p v-for="item in list" :key="item.id">
+      <li>{{ item.id }}</li>
+      <li>{{ item.email }}</li>
+      <li>{{ item.first_name }} {{ item.last_name }}</li>
+      <li>
+        <img :src="item.avatar" />
+      </li>
+    </ul>
+    <!-- <p v-for="item in list" :key="item.id">
     {{item.id}} - {{item.email}} , {{item.first_name}} {{item.last_name}} : {{item.avatar}}
 
   </p> -->
@@ -17,23 +17,25 @@
 </template>
 
 <script>
-  import axios from "axios";
+// API Key: 2831E892-EC06-4D1D-9A75-8D12017D06F8
+// GET /v1/orderbooks/{symbol_id}/latest?limit={limit}&limit_levels={limit_levels}
+// GET /v1/orderbooks/{symbol_id}/history?time_start={time_start}&time_end={time_end}&limit={limit}&limit_levels={limit_levels}
+import axios from "axios";
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   // components: {
   //   // HelloWorld
   // },
-  data()
-  {
+  data() {
     return {
-      list:[]
-    }
+      list: [],
+    };
   },
 
-  async  mounted() {
+  async mounted() {
     let result = await axios.get("https://reqres.in/api/users?page=1");
-    console.log(result.data.data)
-    this.list=result.data.data
+    console.log(result.data.data);
+    this.list = result.data.data;
   },
 };
 </script>
@@ -43,16 +45,16 @@ export default {
   display: flex;
   justify-content: center;
 }
- 
- .item li {
-   display: inline-block;
-   border: 1px solid black;
-   width: 180px;
-   text-align: center;
-   padding: 5px;
-  }
-  
-  .item img {
-    width: 50px;
-  }
-  </style>
+
+.item li {
+  display: inline-block;
+  border: 1px solid black;
+  width: 180px;
+  text-align: center;
+  padding: 5px;
+}
+
+.item img {
+  width: 50px;
+}
+</style>
